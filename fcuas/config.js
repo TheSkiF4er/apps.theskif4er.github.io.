61902ea7c8ee595ee35f268b7ab0ee7058f84aa1
SkiF4er <cajeerskif4er@gmail.com>
2026-03-20 00:52:56 +0300

    Delete fcuas directory

--- fcuas/config.js
+++
@@ -1,6 +0,0 @@
-export const APP_CONFIG = {
-  supabaseUrl: 'https://lmsyycfecaqjyvkjcpbl.supabase.co',
-  supabaseAnonKey: 'sb_publishable_rCuLpBPNLNYdhrDXFG6PJQ_In-G4SBZ',
-  appTitle: 'Таблица частот',
-  defaultColor: '#D32F2F',
-};
