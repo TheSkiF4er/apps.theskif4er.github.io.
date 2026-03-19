eb31dcb2ca477cdd193115b4a0e5603f62854ece
SkiF4er <cajeerskif4er@gmail.com>
2026-03-19 23:37:56 +0300

    Update config.js

--- fcuas/config.js
+++ fcuas/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  supabaseUrl: 'https://YOUR_PROJECT.supabase.co',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
-  appTitle: 'FPV 5.8 GHz Table',
+  supabaseUrl: 'https://lmsyycfecaqjyvkjcpbl.supabase.co',
+  supabaseAnonKey: 'sb_publishable_rCuLpBPNLNYdhrDXFG6PJQ_In-G4SBZ',
+  appTitle: 'FCUAS',
   defaultColor: '#D32F2F',
 };
